f594d4a512c833fd9b45eb7d65bbd61264813164
andreyjardim <andreyjardim@yahoo.com>
2023-05-10 21:02:19 -0400

    Correção para mobile

--- src/Cabecalho.php
+++ src/Cabecalho.php
@@ -16,6 +16,9 @@
 		<LINK REL='stylesheet' href='/css/Padrao.css'>
 		<LINK REL='stylesheet' href='/css/Pergunta.css'>
 		
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+		
 	</HEAD>
 
 
