ce61e9b58eb550ea7c6ec354bc89a710479f8f43
Andrey Bibiano Jardim <88147811+andreyjardim@users.noreply.github.com>
2023-05-17 08:57:19 -0300

    Sobre o sistema

---
+++ src/Sobre.php
@@ -0,0 +1,30 @@
+<?php
+
+	include "Cabecalho.php";
+
+?>
+
+<DIV class='formulario'>	
+	
+<DIV class='titulo_formulario'>Sobre o Cadastro de Pessoas</DIV>
+
+<P>O cadastro de pessoas permite criar um catálogo de pessoas que possa ser categorizado e classificado conforme a demanda do usuário.</P>
+  
+<p>Autores:</P>
+
+<UL>
+    <LI> Andrey Bibiano Jardim </LI> 
+    <LI> Bárbara Gabriela Evangelista Paixão </LI> 
+    <LI> Evellyn Andrade Alves da Silva </LI> 
+    <LI> Leonardo dos Santos </LI> 
+    <LI> Leonardo Ferreira dos Santos </LI> 
+    <LI> Thiago Muniz Severino dos Santos </LI> 
+</UL>
+  
+
+<DIV id='rodape'>
+	<a href='/Pessoas_Listar.php'><img src='/Imagens/VoltarParaListagem.png'></a>
+</DIV>
+
+</DIV>
+
